f24c8fcea0efdacbb89ee96f9800adba1336426d
Illya Sukhomlyn <gollpfd@gmail.com>
2026-07-11 16:56:41 +0300

    update

--- src/components/ContactFormSection/ContactFormSection.tsx
+++ src/components/ContactFormSection/ContactFormSection.tsx
@@ -1,9 +1,26 @@
+"use client";
+
 import Image from "next/image";
 import "./style.css";
 import Link from "next/link";
-import { BsInstagram, BsTelegram, BsThreads } from "react-icons/bs";
+import { PiTelegramLogoLight, PiThreadsLogo } from "react-icons/pi";
+import { FaInstagram } from "react-icons/fa";
+import { useState } from "react";
 
 export function ContactFormSection() {
+  const [formData, setFormData] = useState({
+    name: "",
+    phoneNumber: "",
+    text: "",
+  });
+
+  const changePhoneNumber = (num: string) => {
+    if (!/^\d*$/.test(num)) return;
+
+    if (num.length > 9) return;
+    setFormData({ ...formData, phoneNumber: num });
+  };
+
   return (
     <section className="contact-form-section">
       <div className="container">
@@ -31,30 +48,96 @@ export function ContactFormSection() {
               <div className="contact-form-section-content-row">
                 <span>Email</span>
                 <Link
-                  href={"/"}
+                  href="mailto:vektra.it.agency@gmail.com"
                   className="contact-form-section-content-row-title"
+                  data-cursor="hover"
                 >
                   vektra.it.agency@gmail.com
                 </Link>
               </div>
+
               <div className="contact-form-section-content-row">
                 <span>Номер телефону</span>
                 <Link
-                  href={"/"}
+                  href="tel:+380500521571"
                   className="contact-form-section-content-row-title"
+                  data-cursor="hover"
                 >
-                  + 38 ( 050 ) 052 - 15 - 71
+                  +38 ( 050 ) 052 - 15 - 71
                 </Link>
               </div>
               <div className="contact-form-section-content-row">
                 <span>Vektra у digital-просторі</span>
                 <div className="contact-form-section-content-row-social">
-                  <BsTelegram />
-                  <BsInstagram />
-                  <BsThreads />
+                  <Link
+                    href="https://t.me/iLyhaha1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="contact-form-section-content-row-social-link"
+                    data-cursor="hover"
+                  >
+                    <PiTelegramLogoLight className="contact-form-section-content-row-social-icon" />
+                  </Link>
+
+                  <Link
+                    href="https://www.instagram.com/vektra_agency/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="contact-form-section-content-row-social-link"
+                    data-cursor="hover"
+                  >
+                    <FaInstagram className="contact-form-section-content-row-social-icon" />
+                  </Link>
+
+                  <Link
+                    href="https://www.threads.com/@vektra_agency"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="contact-form-section-content-row-social-link"
+                    data-cursor="hover"
+                  >
+                    <PiThreadsLogo className="contact-form-section-content-row-social-icon" />
+                  </Link>
                 </div>
               </div>
             </div>
+          </div>
+          <div className="contact-form-section-content-right">
+            <form className="contact-form-section-content-right-form">
+              <div className="contact-form-section-content-right-form-group">
+                <label htmlFor="name">Ім’я</label>
+                <input
+                  type="text"
+                  id="name"
+                  required
+                  placeholder="Як до Вас звертатись?"
+                  value={formData.name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, name: e.target.value })
+                  }
+                />
+              </div>
+              <div className="contact-form-section-content-right-form-group">
+                <label htmlFor="phone">Номер телефону</label>
+                <div className="ContactForm-wrapper-form-group-phone">
+                  <label
+                    htmlFor="phone"
+                    className="ContactForm-wrapper-form-group-phone-label"
+                  >
+                    +380
+                  </label>
+                  <input
+                    type="text"
+                    id="phone"
+                    placeholder="00-000-0000"
+                    className="ContactForm-wrapper-form-group-phone-input"
+                    required
+                    value={formData.phoneNumber}
+                    onChange={(e) => changePhoneNumber(e.target.value)}
+                  />
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
