950480f568745485cd48ee6c422d86ab84ce7833
Illya Sukhomlyn <gollpfd@gmail.com>
2026-07-12 21:50:32 +0300

    update

--- src/components/ContactFormSection/ContactFormSection.tsx
+++ src/components/ContactFormSection/ContactFormSection.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { PiTelegramLogoLight, PiThreadsLogo } from "react-icons/pi";
 import { FaInstagram } from "react-icons/fa";
 import { useState } from "react";
+import { toast } from "sonner";
 
 export function ContactFormSection() {
   const [formData, setFormData] = useState({
@@ -14,11 +15,59 @@ export function ContactFormSection() {
     text: "",
   });
 
+  const [loading, setLoading] = useState(false);
+
   const changePhoneNumber = (num: string) => {
     if (!/^\d*$/.test(num)) return;
 
     if (num.length > 9) return;
     setFormData({ ...formData, phoneNumber: num });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (formData.phoneNumber.length !== 9) {
+      toast.error("Введіть коректний номер телефону");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await fetch("/api/leed/create-leed", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Помилка при відправці");
+      }
+
+      toast("Дякуємо за звернення!", {
+        description:
+          "Ми отримали вашу заявку. Найближчим часом зв'яжемося з вами.",
+      });
+
+      setFormData({
+        name: "",
+        phoneNumber: "",
+        text: "",
+      });
+    } catch (error) {
+      console.error(error);
+
+      toast.error(
+        error instanceof Error ? error.message : "Сталася невідома помилка",
+      );
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -103,40 +152,65 @@ export function ContactFormSection() {
             </div>
           </div>
           <div className="contact-form-section-content-right">
-            <form className="contact-form-section-content-right-form">
-              <div className="contact-form-section-content-right-form-group">
-                <label htmlFor="name">Ім’я</label>
-                <input
-                  type="text"
-                  id="name"
-                  required
-                  placeholder="Як до Вас звертатись?"
-                  value={formData.name}
-                  onChange={(e) =>
-                    setFormData({ ...formData, name: e.target.value })
-                  }
-                />
-              </div>
-              <div className="contact-form-section-content-right-form-group">
-                <label htmlFor="phone">Номер телефону</label>
-                <div className="ContactForm-wrapper-form-group-phone">
-                  <label
-                    htmlFor="phone"
-                    className="ContactForm-wrapper-form-group-phone-label"
-                  >
-                    +380
-                  </label>
+            <form
+              className="contact-form-section-content-right-form"
+              onSubmit={handleSubmit}
+            >
+              <div className="contact-form-section-content-right-form-row">
+                <div className="contact-form-section-content-right-form-group">
+                  <label htmlFor="section-name">Ім’я</label>
                   <input
                     type="text"
-                    id="phone"
-                    placeholder="00-000-0000"
-                    className="ContactForm-wrapper-form-group-phone-input"
+                    id="section-name"
                     required
-                    value={formData.phoneNumber}
-                    onChange={(e) => changePhoneNumber(e.target.value)}
+                    placeholder="Як до Вас звертатись?"
+                    value={formData.name}
+                    onChange={(e) =>
+                      setFormData({ ...formData, name: e.target.value })
+                    }
                   />
                 </div>
+                <div className="contact-form-section-content-right-form-group">
+                  <label htmlFor="section-phone">Номер телефону</label>
+                  <div className="contact-form-section-content-right-form-group-phone">
+                    <label
+                      htmlFor="section-phone"
+                      className="contact-form-section-content-right-form-group-phone-label"
+                    >
+                      +380
+                    </label>
+                    <input
+                      type="text"
+                      id="section-phone"
+                      placeholder="00-000-0000"
+                      className="contact-form-section-content-right-form-group-phone-input"
+                      required
+                      value={formData.phoneNumber}
+                      onChange={(e) => changePhoneNumber(e.target.value)}
+                    />
+                  </div>
+                </div>
               </div>
+              <div className="contact-form-section-content-right-form-group">
+                <label htmlFor="section-request">Запит</label>
+                <textarea
+                  name="text"
+                  id="section-request"
+                  placeholder="Розкажіть про ваш проєкт, задачу або ідею"
+                  rows={5}
+                  value={formData.text}
+                  onChange={(e) =>
+                    setFormData({ ...formData, text: e.target.value })
+                  }
+                ></textarea>
+              </div>
+              <button
+                style={{ opacity: `${loading ? "0.8" : "1"}` }}
+                className="contact-form-section-content-right-form-button"
+                data-cursor="hover"
+              >
+                {loading ? "Зачекайте ..." : "Обговорити проєкт"}
+              </button>
             </form>
           </div>
         </div>
